ad6bfb2c1eb6e5da5ac86917f35e098ec0c4ca96
DevonK <devon.kup@gmail.com>
2013-02-15 17:16:05 -0600

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GigaBlab::Application.config.secret_token = '6ccffeac5ee6dbaf49a06ab371760b83651d5a6d17234d89c7318eaa9ee57cc3298ed2a183a81e3b883a487b4d66a5d68b2125ca31c0f1169ad7ac6750a7df44'
