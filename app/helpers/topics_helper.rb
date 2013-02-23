42726787164ac12410fb12d79f5fe9a497d112b4
DevonK <devon.kup@gmail.com>
2013-02-22 19:40:44 -0600

    Added timestamp helper

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -2,4 +2,11 @@ module TopicsHelper
   def join_tags(topic)
     topic.tags.map { |t| t.name }.join(", ")
   end
+  def date_helper(timestamp)
+    if timestamp > 24.hours.ago
+      time_ago_in_words(timestamp)
+    else
+      (timestamp.strftime('%-d-%-m-%Y') + tag(:br) + timestamp.strftime('%H:%M:%S')).html_safe
+    end
+  end
 end
