c15e6115fd34bb353e8fee519d502c59ee61af46
DevonK <devon.kup@gmail.com>
2013-02-27 19:54:39 -0600

    Added default admin account

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = User.create(:username => "admin", :email => "admin", :password => "admin", :password_confirmation => "admin")
+admin.update_attribute(:admin, 1)
