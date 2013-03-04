ef3524846a35e2f06b1621a33cd33c7fc951265f
DevonK <devon.kup@gmail.com>
2013-03-03 21:04:58 -0600

    Quick fix

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -2,4 +2,12 @@ module TopicsHelper
   def join_tags(topic)
     topic.tags.map { |t| t.name }.join(", ")
   end
+
+  def last_post_helper(topic)
+    if topic.posts.count > 0
+      topic.posts.last.created_at
+    else
+      topic.created_at
+    end
+  end
 end
