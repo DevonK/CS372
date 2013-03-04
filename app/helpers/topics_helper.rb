0da5f75b3f10a5c2012afb4e7a90ff5a05797fa3
DevonK <devon.kup@gmail.com>
2013-03-03 20:03:22 -0600

    Modified timestamps

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -2,18 +2,23 @@ module TopicsHelper
   def join_tags(topic)
     topic.tags.map { |t| t.name }.join(", ")
   end
-  def date_helper(timestamp)
+  def date_helper(timestamp, flag)
     if timestamp > 24.hours.ago
       time_ago_in_words(timestamp)
     else
-      (timestamp.strftime('%-d-%-m-%Y') + tag(:br) + timestamp.strftime('%H:%M:%S')).html_safe
+      if flag == 1
+        time_space = " - "
+      else
+        time_space = tag(:br)
+      end
+      (timestamp.strftime('%-d-%-m-%Y') + time_space + timestamp.strftime('%I:%M:%S %p')).html_safe
     end
   end
   def last_post_helper(topic)
     if topic.posts.count > 0
-      date_helper(topic.posts.last.created_at)
+      date_helper(topic.posts.last.created_at, 0)
     else
-      date_helper(topic.created_at)
+      date_helper(topic.created_at, 0)
     end
   end
 end
