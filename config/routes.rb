76e94bb47fffda171696fa48ac6a66e68df149e8
Devon Kupiec <devon.kup@gmail.com>
2013-03-04 15:28:26 -0600

    Added basic user profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ GigaBlab::Application.routes.draw do
   root :to => "home#index"
 
   devise_for :users
+  match 'users/:id' => 'users#show'
 
   resources :topics do
     resources :posts, :only => [:create, :destroy]
