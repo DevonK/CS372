0025db2b14bfdb7d0538b6a50d7cd9b187b3c405
DevonK <devon.kup@gmail.com>
2013-03-17 16:02:34 -0600

    Made homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
+  respond_to :html, :json
+
   def index
+    @topics = Topic.last(2)
+    @users = User.last(2)
+    @posts = Post.last(2)
+    respond_with @topics
   end
 end
